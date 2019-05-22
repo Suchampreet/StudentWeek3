56ca247eaa448220094fa972e7f8839d4adc0b00
Suchampreet <sinsuch@sheridancollege.ca>
2019-05-22 09:37:43 -0400

    Initial commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author jujha
+ */
+public class Student {
+    private int id;
+    private String firstName;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+}
---
+++ src/studentweek3/StudentWeek3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author jujha
+ */
+public class StudentWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Student s1=new Student();
+        s1.setFirstName("John");
+        s1.setId(4);
+    }
+    
+}
